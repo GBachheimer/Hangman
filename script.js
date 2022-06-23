abe484b164cc2519b0deb7c6a6aafba61bbbda71
GBachheimer <gbachheimer@icloud.com>
2022-06-23 12:38:56 +0300

    started to implement basic functions

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 function generateGame(word) {
-    let wordLength = Number(word.length);
-    for (let i = 1; i < wordLength; ++i) {
-        let dash = document.createElement("svg");
-        dash.xmlns = "http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor";
-        dash.classList = "bi bi-dash-lg";
-        dash. viewBox = "0 0 16 16";
-        let path = document.createElement("path");
-        path.fill-rule = "evenodd";
-        path.d = "M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z";
-        dash.appendChild(path);
-        documnet.getElementById("gameBoard").appendChild(dash);
-    }
+    document.getElementById("word").disabled = true;
+    document.getElementById('word').value = "";
+    document.getElementById("gameBoard").innerHTML = word;
+    document.getElementById("button").innerHTML = "Reset";
+    document.getElementById("button").onclick = "reset()";
+    document.getElementById("inputArea").classList.remove("top-50");
+    document.getElementById("inputArea").className += " top-0 translate-middle-x";
 }
