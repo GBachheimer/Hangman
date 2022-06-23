a39d38f3e69b4f773061f2af9d19be1eb25750ab
GBachheimer <gbachheimer@icloud.com>
2022-06-23 22:32:46 +0300

    makeup beauty work

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function generateGame(word) {
     const gameArray = [];
     let wordLength = word.length;
     for (let i = 1; i <= wordLength; ++i) {
-        gameArray.push(" ");
+        gameArray.push(" • ");
     }
     document.getElementById('word').value = ""; //empty input
     document.getElementById("gameBoard").innerHTML = gameArray.join(" | "); //display bars
@@ -15,10 +15,11 @@ function generateGame(word) {
         }; //change start event
     let newButton = document.createElement("button"); //create reset button
     newButton.innerHTML = "Reset the game";
-    newButton.className = "btn-sm btn-primary";
+    newButton.className = "btn-sm btn-info my-2";
     newButton.onclick = function () { reset(); };
     document.getElementById("form").appendChild(newButton);
     let message = document.createElement("p"); //create word length message
+    message.id = "info";
     message.innerHTML = "The word has " + wordLength + " letters";
     document.getElementById("form").appendChild(message);
 }
@@ -31,27 +32,28 @@ function checkLetter(letter, word, gameArray) {
     let wordLength = word.length;
     let foundIndex = word.indexOf(letter);
     if (foundIndex == -1) {
-        document.getElementById("foundOrNot").innerHTML = "Wrong letter!";
+        
         decreaseLifes();
     } else {
         while (foundIndex > -1) {
-            gameArray[foundIndex] = letter;
+            gameArray[foundIndex] = letter.toUpperCase();
             foundIndex = word.indexOf(letter, foundIndex + 1);
             ++countWin;
         }
         document.getElementById("gameBoard").innerHTML = gameArray.join(" | ");
-        document.getElementById("foundOrNot").innerHTML = "Well done! " + (wordLength - countWin) + " more letters to guess.";
+        document.getElementById("lifes").innerHTML = "Well done! " + (wordLength - countWin) + " more letters to guess.";
     }
     winOrLose(wordLength, word);
 }
 
-function decreaseLifes () {
+function decreaseLifes() {
     lifes--;
-    document.getElementById("lifes").innerHTML = "You have " + lifes + " more attempts!";
+    document.getElementById("lifes").innerHTML = "Wrong letter! You have " + lifes + " more attempts!";
 }
 
 function winOrLose(wordLength, word) {
-    let message = document.createElement("p");
+    let message = document.createElement("h1");
+    message.classList = "text-warning";
     if (countWin === wordLength) {
         message.innerHTML = "YOU WON!";
         display(message);
@@ -62,16 +64,16 @@ function winOrLose(wordLength, word) {
 }
 
 function display(message) {
-    document.getElementById("foundOrNot").innerHTML = "";
-    document.getElementById("inputArea").appendChild(message);
-    document.getElementById("word").disabled = true;
-    document.getElementById("button").disabled = true;
+    document.getElementById("centralDiv").appendChild(message);
+    document.getElementById("word").style.display = "none";
+    document.getElementById("button").style.display = "none";
+    document.getElementById("lifes").style.display = "none";
+    document.getElementById("info").style.display = "none";
 }
 
 function checkWordPattern(word) {
     const wordPattern = /^.[a-z]+$/;
     if (wordPattern.test(word)) {
-        document.getElementById("lifes").innerHTML = "Enter a single letter:";
         generateGame(word);
     } else {
         document.getElementById("lifes").innerHTML = "Please enter a word!";
