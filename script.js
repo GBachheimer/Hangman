2f7e772c3d3c64f0fbf5086ce040aa9f35d17211
GBachheimer <gbachheimer@icloud.com>
2022-06-23 16:04:24 +0300

    reset function, input & buttons refined

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 function generateGame(word) {
-    document.getElementById("word").disabled = true;
-    document.getElementById('word').value = "";
-    document.getElementById("gameBoard").innerHTML = word;
-    document.getElementById("button").innerHTML = "Reset";
-    document.getElementById("button").onclick = "reset()";
-    document.getElementById("inputArea").classList.remove("top-50");
-    document.getElementById("inputArea").className += " top-0 translate-middle-x";
+    const gameArray = [];
+    let wordLength = word.length;
+    for (let i = 1; i <= wordLength; ++i) {
+        gameArray.push(" ");
+    }
+    document.getElementById('word').value = ""; //empty input
+    document.getElementById("gameBoard").innerHTML = gameArray.join(" | "); //display bars
+    document.getElementById("button").innerHTML = "Check this letter"; //change start button
+    document.getElementById("button").onclick = function () {
+            checkLetter(document.getElementById('word').value.toLowerCase(), word, gameArray);
+        }; //change start event
+    let newButton = document.createElement("button"); //create reset button
+    newButton.innerHTML = "Reset";
+    newButton.className = "btn-sm btn-primary";
+    newButton.onclick = function () { reset(); };
+    document.getElementById("form").appendChild(newButton);
+    let message = document.createElement("p"); //create word length message
+    message.innerHTML = "The word has " + wordLength + " letters";
+    document.getElementById("form").appendChild(message);
 }
+
+function reset() {
+    location.reload();
+}
+
