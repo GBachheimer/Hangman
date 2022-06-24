dd32964da114f676fea83fb454a5143b7bd9c826
GBachheimer <107429234+GBachheimer@users.noreply.github.com>
2022-06-24 18:19:50 +0300

    corrections

--- script.js
+++ script.js
@@ -32,8 +32,7 @@ function checkLetter(letter, word, gameArray) {
     let wordLength = word.length;
     let foundIndex = word.indexOf(letter);
     if (foundIndex == -1) {
-        
-        decreaseLifes();
+        decreaseLives();
     } else {
         while (foundIndex > -1) {
             gameArray[foundIndex] = letter.toUpperCase();
@@ -45,14 +44,14 @@ function checkLetter(letter, word, gameArray) {
         if (wordLength - countWin <= 1) {
             correction = "letter";
         }
-        document.getElementById("lifes").innerHTML = "Well done! " + (wordLength - countWin) + " more " + correction + " to guess.";
+        document.getElementById("lives").innerHTML = "Well done! " + (wordLength - countWin) + " more " + correction + " to guess.";
     }
     winOrLose(wordLength, word);
 }
 
-function decreaseLifes() {
-    lifes--;
-    document.getElementById("lifes").innerHTML = "Wrong letter! You have " + lifes + " more attempts!";
+function decreaseLives() {
+    --lives;
+    document.getElementById("lives").innerHTML = "Wrong letter! You have " + lives + " more attempts!";
 }
 
 function winOrLose(wordLength, word) {
@@ -61,7 +60,7 @@ function winOrLose(wordLength, word) {
     if (countWin === wordLength) {
         message.innerHTML = "YOU WON!";
         display(message);
-    } else if (lifes === 0) {
+    } else if (lives === 0) {
         message.innerHTML = "YOU LOSE! The word was: " + word.toUpperCase() + ".";
         display(message);
     }
@@ -71,18 +70,18 @@ function display(message) {
     document.getElementById("centralDiv").appendChild(message);
     document.getElementById("word").style.display = "none";
     document.getElementById("button").style.display = "none";
-    document.getElementById("lifes").style.display = "none";
+    document.getElementById("lives").style.display = "none";
     document.getElementById("info").style.display = "none";
 }
 
 function checkWordPattern(word) {
     const wordPattern = /^.[a-z]+$/;
     if (wordPattern.test(word)) {
-        document.getElementById("lifes").innerHTML = "Player 2 guess the word letter by letter. Good luck!";
+        document.getElementById("lives").innerHTML = "Player 2 guess the word letter by letter. Good luck!";
         generateGame(word);
         document.getElementById("word").type = "text";
     } else {
-        document.getElementById("lifes").innerHTML = "Please enter a word!";
+        document.getElementById("lives").innerHTML = "Please enter a word!";
     }
 }
 
@@ -91,6 +90,6 @@ function checkLetterPattern(letter, word, gameArray) {
     if (letterPattern.test(letter) && letter != "" && !gameArray.includes(letter.toUpperCase())) {
         checkLetter(letter, word, gameArray);
     } else {
-        document.getElementById("lifes").innerHTML = "Please enter a single letter, different from the ones guessed so far!";
+        document.getElementById("lives").innerHTML = "Please enter a single letter, different from the ones guessed so far!";
     }
 }
